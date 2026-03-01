7f7c8dd1537890fcd927a9d3fb991f5af91b8315
Documenter.jl <documenter@juliadocs.github.io>
2026-03-01 18:01:03 +0000

    build based on 8011a4f

--- dev/pages/tutorials/specular.jl
+++ dev/pages/tutorials/specular.jl
@@ -1,10 +1,8 @@
 # # Specular Canopy Scattering
 
 # Using packages:
-using Plots, Distributions
+using CairoMakie, Distributions
 using CanopyOptics
-pyplot()
-# theme(:ggplot2)
 
 # Create a specular model with refractive index and "roughness" κ
 specularMod = CanopyOptics.SpecularCanopyScattering(nᵣ=1.5, κ=0.2)
@@ -14,37 +12,63 @@ LD = CanopyOptics.planophile_leaves2()
 
 # Create some discretized angles:
 # Azimuth (in radians)
-ϕ = range(0.0, 2π,  length=200);
+ϕ = range(0.0, 2π, length=200);
 # Polar angle as cos(Θ)
-μ,w = CanopyOptics.gauleg(180,0.0,1.0);
+μ, w = CanopyOptics.gauleg(180, 0.0, 1.0);
 # Create directional vectors:
-dirs = [CanopyOptics.dirVector_μ(a,b) for a in μ, b in ϕ];
+dirs = [CanopyOptics.dirVector_μ(a, b) for a in μ, b in ϕ];
 
-# Compute specular reflectance:
+# Compute specular reflectance for a fixed incoming direction:
 R = CanopyOptics.compute_reflection.([specularMod], [dirs[50,1]], dirs, [LD]);
 
-# Polar plot of reflectance
-contourf(ϕ, acos.(μ), R,proj=:polar, ylim=(0,π/2), alpha=0.8)
+# ### Polar plot of reflectance using PolarAxis
+# Zenith angle θ is the radial axis (0 = nadir, π/2 = horizon).
+# acos.(μ) is descending (μ ascending); sort to ascending and reverse R rows to match.
+# PolarAxis surface! expects matrix shape (n_angle × n_radius).
+θ       = sort(acos.(μ))            # ascending, 0 → π/2
+R_polar = reverse(R, dims=1)'       # (nϕ=200 × nθ=180)
+
+fig = Figure(size=(520, 450))
+ax  = PolarAxis(fig[1, 1], title="Reflectance R")
+hm  = surface!(ax, collect(ϕ), θ, zeros(size(R_polar)),
+               color=R_polar, colorrange=(0, 0.02), shading=NoShading)
+rlims!(ax, 0, π/2)
+Colorbar(fig[1, 2], hm)
+fig
 
 # ### Animation over different Beta leaf distributions
-steps = 0.1:0.1:5 
-α = [collect(steps); 5*ones(length(steps))] 
-β = [ 5*ones(length(steps)); reverse(collect(steps));] 
-x = 0:0.01:1
-l = @layout [a  b; c d; e f]
+steps  = 0.1:0.2:5
+α_vals = [collect(steps); 5 * ones(length(steps))]
+β_vals = [5 * ones(length(steps)); reverse(collect(steps))]
+x      = collect(0:0.01:1)
 
-anim = @animate for i ∈ eachindex(α)
-    LD = CanopyOptics.LeafDistribution(Beta(α[i],β[i]), 2/π)
-    R = CanopyOptics.compute_reflection.([specularMod], [dirs[120,1]], dirs, [LD])
-    pZ = []
-    for m=0:3
-        Z⁺⁺, Z⁻⁺ = CanopyOptics.compute_Z_matrices(specularMod, μ, LD, m)
-        push!(pZ, contourf(μ, μ, Z⁻⁺, title="Z⁻⁺ Fourier moment $m", xlabel="μꜜ", aspect_ratio = :equal, ylabel="μꜛ"))
-    end
-    p0 = plot(rad2deg.(π * x/2), pdf.(LD.LD,x), legend=false, ylim=(0,3), title="Leaf angle distribution", xlabel="Θ (degrees)")
-    p1 = contourf(ϕ, acos.(μ), R, proj=:polar, ylim=(0,π/2), clims=(0,0.02), alpha=0.8,legend = :none, title="Polar plot R")
-    plot(p0, p1, pZ..., layout = l, margin=5Plots.mm)
-    plot!(size=(700,700))
+# Observables: updating LD_obs propagates to all derived plots reactively.
+# R has shape (nμ=180, nϕ=200); for heatmap(ϕ, θ, z) Makie needs z of shape (nϕ, nθ).
+LD_obs  = Observable(LD)
+pdf_obs = @lift pdf.($LD_obs.LD, x)
+R_obs   = @lift reverse(
+    CanopyOptics.compute_reflection.([specularMod], [dirs[120,1]], dirs, [$LD_obs]),
+    dims=1)'
+# Use map to properly capture m in each iteration (avoids closure-in-loop)
+Zpm_obs = map(0:3) do m
+    @lift CanopyOptics.compute_Z_matrices(specularMod, μ, $LD_obs, m)[2]
 end
 
-gif(anim, "anim_fps10.gif", fps = 10)
+fig2 = Figure(size=(700, 700))
+ax0  = Axis(fig2[1,1], title="Leaf angle distribution",
+            xlabel="Θ (degrees)", limits=(0, 90, 0, 3))
+ax1  = Axis(fig2[1,2], title="Reflectance R",
+            xlabel="Azimuth ϕ (°)", ylabel="Zenith θ (°)")
+ax_Z = [Axis(fig2[i+1, j], title="Z⁻⁺ m=$(2(i-1)+(j-1))",
+             xlabel="μꜜ", ylabel="μꜛ") for i in 1:2, j in 1:2]
+
+lines!(ax0, rad2deg.(π * x / 2), pdf_obs)
+heatmap!(ax1, rad2deg.(collect(ϕ)), rad2deg.(θ), R_obs, colorrange=(0, 0.02))
+for (k, ax) in enumerate(ax_Z)
+    heatmap!(ax, μ, μ, Zpm_obs[k], colorrange=(0, 0.3))
+end
+
+record(fig2, "anim_fps10.gif", eachindex(α_vals); framerate=10) do i
+    LD_obs[] = CanopyOptics.LeafDistribution(Beta(α_vals[i], β_vals[i]), 2/π)
+end
+fig2
